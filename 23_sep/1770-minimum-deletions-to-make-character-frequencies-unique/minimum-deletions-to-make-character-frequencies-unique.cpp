56f1d0d0ba38faa2615ef65c92a61a44c6f3b920
MahmoudEl-Husseni <67474135+MahmoudEl-Husseni@users.noreply.github.com>
2023-09-12 12:06:26 +0300

    Time: 50 ms (77.52%) | Memory: 17.2 MB (77.52%) - LeetSync

--- 23_sep/1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 23_sep/1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -3,14 +3,11 @@ public:
     int minDeletions(string s) {
         int ans = 0;
         vector<int> freq(26);
-        set<int> st;
         for(auto c : s){
             freq[c-'a']++;
         }
         sort(freq.begin(), freq.end());
         for(int i=24;i>=0;i--){
-            if(freq[i]==0)
-                continue;
             if(freq[i]>=freq[i+1]){
                 int d = freq[i]-freq[i+1]+(freq[i+1]!=0);
                 ans+=d;
@@ -20,6 +17,3 @@ public:
         return ans;
     }
 };
-
-// 1 1 1 4
-// 0 0 1 4
