592773d4fceaa14ea9516a091b1ea4c13ddaf928
MahmoudEl-Husseni <oudm753@gmail.com>
2023-10-31 20:19:40 +0200

    2433. Find The Original Array of Prefix Xor: CPP Solution added

---
+++ 23_oct/2433. Find The Original Array of Prefix Xor/2433. Find The Original Array of Prefix Xor.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> ans = pref;
+        for(int i=1;i<ans.size();i++){
+            ans[i] = pref[i]^pref[i-1];
+        }
+        return ans;
+    }
+};
